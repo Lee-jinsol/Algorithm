77ee7cf07e890344c7aff28ae79a05c48ff1d551
jinsol9419@naver.com <jinsol9419@naver.com>
2022-02-09 15:00:33 +0900

    Solve 곱하기 혹은 더하기 문제

--- Algorithm/greedy/곱하기 혹은 더하기.py
+++ Algorithm/greedy/곱하기 혹은 더하기.py
@@ -1,1 +1,17 @@
 # 곱하기 혹은 더하기 문제 
+
+# 수가 0이나 1인경우 더한다
+# 두수가 모두 2이상인 경우에 곱한다
+
+data = input()
+
+result = int(data[0])
+
+for i in range(1, len(data)):
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)
