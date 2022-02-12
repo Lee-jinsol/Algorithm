a21910beb62d938eaec1905b67bd662e1e73aa57
이진솔 <jinsol9419@naver.com>
2022-02-12 22:45:10 +0900

    Add code : 설탕배달 solve

---
+++ Algorithm/greedy/설탕배달.py
@@ -0,0 +1,37 @@
+'''
+    suger = int(input())
+    result = 0
+    multi = 0
+    rangeCoin = suger // 3
+
+    if (suger % 5) == 0 :
+        result = suger // 5
+    else :
+        for i in range (rangeCoin):
+            multi = suger -(3*(i+1))
+            result += 1
+            if (multi % 5) == 0:
+                result += (multi//5)
+                multi = 0
+                break
+        if multi != 0:
+            result = -1
+        
+    print(result)
+'''
+suger = int(input())
+result = 0
+
+while suger > 0:
+    if (suger % 5) == 0:
+        result += suger // 5
+        suger = 0
+        break
+    suger -= 3
+    result += 1
+
+if suger != 0:
+    result = -1
+
+
+print(result)
