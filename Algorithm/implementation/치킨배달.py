d8876ba8c42c9c1aa9325995e02ca742129eb6ee
jinsol9419@naver.com <jinsol9419@naver.com>
2022-02-17 16:47:32 +0900

    치킨배달 풀이 완료

--- Algorithm/implementation/치킨배달.py
+++ Algorithm/implementation/치킨배달.py
@@ -11,11 +11,15 @@ for i in range(n):
     for j in range(n):
         if board[i][j] == 1 : 
             house.append((i,j))
-        else if [i][j] == 2 : 
+        if board.[i][j] == 2 : 
             chicken.append((i,j))
 
+minv = float('inf')
 for ch in combinations(chicken, run_chick):
-    sum = 0
+    sumv = 0
     for home in house:
-        sum += min([abs(home[0]-i[0])+abs(home[1]-i[1]) for i in ch])
-        if 
+        sumv += min([abs(home[0]-i[0])+abs(home[1]-i[1]) for i in ch])
+        if minv <= sumv: break
+    if sumv < minv: minv = sumv
+
+print(minv)
