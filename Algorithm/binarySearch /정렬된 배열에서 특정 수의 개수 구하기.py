299dbaaf48317642c09dc536938f70402ddd0dfc
jinsol9419@naver.com <jinsol9419@naver.com>
2022-02-22 20:33:37 +0900

    정렬된 배열에서 특정 수의 개수 구하기 문제 풀이 완료

---
+++ Algorithm/binarySearch /정렬된 배열에서 특정 수의 개수 구하기.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left, bisect_right
+
+def count_by_range(array, left_value, right_value):
+    left_index = bisect_left(array, left_value)
+    right_index = bisect_right(array, right_value)
+    return right_index - left_index
+
+n,x = map(int, input().split())
+a = list(map(int, input().split()))
+
+count = count_by_range(a, x, x)
+if count == 0:
+    print(-1)
+else:
+    print(count)
