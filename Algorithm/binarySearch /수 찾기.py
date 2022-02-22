dd9d0a5e6a21a2fc60c98fdc8c7fb92d3bb10610
jinsol9419@naver.com <jinsol9419@naver.com>
2022-02-22 21:56:03 +0900

    수찾기 문제 입력받는 부분 구현완료

---
+++ Algorithm/binarySearch /수 찾기.py
@@ -0,0 +1,15 @@
+n = int(input().split())
+n_list = list(map(int, input().split()))
+m = int(input().split())
+m_list = list(map(int, input().split()))
+
+result = []
+
+def binary_search(array)
+
+1 2 3 4 5 
+1 3 5 7 9
+
+
+for _ in result:
+    print(result)
