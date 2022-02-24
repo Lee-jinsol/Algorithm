3cc38688894032b6f912265d0ae04b945931ade8
jinsol9419@naver.com <jinsol9419@naver.com>
2022-02-24 16:07:28 +0900

    수 찾기 문제 구현 완료

--- Algorithm/binarySearch /수 찾기.py
+++ Algorithm/binarySearch /수 찾기.py
@@ -1,16 +1,21 @@
-n = int(input().split())
+n = int(input())
 n_list = list(map(int, input().split()))
-m = int(input().split())
+m = int(input())
 m_list = list(map(int, input().split()))
+n_list.sort()
 
-result = []
+def binarysearch(k, n_list, start, end):
+    if start > end:
+        return 0
+    middle = (start +end) // 2
+    if k == n_list[middle]:
+        return 1
+    elif k < n_list[middle]:
+        return binarysearch(k, n_list, start, middle-1)
+    else:
+        return binarysearch(k, n_list, middle+1, end)
 
-def binary_search(array)
-if 존재하면 :
-    result.append(1)
-else:
-    result.append(0)
-
-
-for _ in result:
-    print(result)
+for k in m_list:
+    start = 0
+    end = len(n_list)-1
+    print(binarysearch(k, n_list, start, end))
